2c3fe4562b52f35b30669bbe0c5338f902dfbfed
kgfsudio <kgfstudio7@gmail.com>
2026-05-26 05:41:13 +0000

    correction du git

--- D:\BankVi\admin/.next/dev/static/chunks/lib_mock_ts_01f584f._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/lib_mock_ts_0olo25i._.js"],
-    source: "dynamic"
-});
